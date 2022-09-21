95fad46fe0a58800c73776ba4f07574aa71f055d
Robi Mustsaar <robimustsaar@gmail.com>
2022-09-21 14:04:10 +0300

    All Items, Done items, Not Done Items

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-    <button class="button" v-on:click="count++">{{ count }}</button>
-    <input type="text" v-model="name">
-  {{ name }}
-    <button class="button" @click="items.push(name)">Add</button>
+  <div class="container">
+    <section class="section">
+      <div class="field has-addons">
+        <p class="control">
+        <input class="input" type="text" v-model="newItem" @keydown.enter = "addItem()">
+        </p>
+        <p class="control">
+      <button class="button" @click="addItem()">Add</button>
+        </p>
+      </div>
+      <div class="content">
+        <h1>All Items</h1>
+    <ul>
+      <li v-for=" item in items " :key="item.id">
+        {{ item.name }}
+        <input type="checkbox" v-model ="item.done">
+      </li>
+    </ul>
+    
+    <h1>Done Items</h1>
+    <ul>
+      <li v-for=" item in DoneItems " :key="item.id">
+        {{ item.name }}
+        <input type="checkbox" v-model ="item.done">
+      </li>
+    </ul>
 
-  <ul>
-    <li v-for="(item,index) in items" :key="index">{{ item }}</li>
-  </ul>
-</template>
+    <h1>Not Done Items</h1>
+    <ul>
+      <li v-for=" item in notDoneItems " :key="item.id">
+        {{ item.name }}
+        <input type="checkbox" v-model ="item.done">
+      </li>
+    </ul>
 
-<script>
-export default {
-    data(){
-        return {
-            name: 'Robi Mustsaar',
-            count: 0,
-            items: ['piim', 'sai', 'viin','õlu']
+      </div>
+    </section>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      data(){
+          return{
+              newItem: '',
+              count: 0,
+              items: [
+                {name:'Saku Ice', done: true, id: 0},
+                {name:'Hoggys Gin', done: false, id: 1},
+                {name:'J2ger', done: false, id: 2},
+              ]
+          }
+      },
+      methods: {
+        addItem(){
+          if(this.newItem !=""){
+          this.items.push({name:this.newItem, done: false, id: this.items.length});
+          }
+          this.newItem = '';
         }
+      },
+      computed: {
+        DoneItems(){
+          return this.items.filter(item => item.done);
+        },
+        notDoneItems(){
+          return this.items.filter(item => !item.done);
+      }
     }
-}
-</script>
-
-<style>
-
-</style>
+  }
+  </script>
+  
+  <style>
+  
+  </style>
