c7793caa8c21a3eab9617cd43b2f3e2af932a18b
Robi Mustsaar <robimustsaar@gmail.com>
2022-10-04 14:14:12 +0300

    Tra koik on sassis juba aga tegime mingit RNM tegelasi

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -6,18 +6,68 @@
         <b>{{displayCps}} Juiceboxes per second</b>
         <img @click="juiceboxClick()" :class="{'click': clickClass}"  src="https://clipart.world/wp-content/uploads/2022/06/Juice-Box-Clipart-Transparent.png">
     </div>
-    <div class="column">
-        <button @click="upgrade(0.1,10)" class="button" :disabled="juiceboxes <=9">Buy 0.1 cps for 10 juiceboxes</button>
-        <button @click="upgrade(1,100)" class="button" :disabled="juiceboxes <=99">Buy 1 cps for 100 juiceboxes</button>
-        <button @click="upgrade(10,1000)" class="button" :disabled="juiceboxes <=999">Buy 10 cps for 1000 juiceboxes</button>
-        <button @click="upgrade(100,10000)" class="button" :disabled="juiceboxes <=9999">Buy 100 cps for 10000 juiceboxes</button>
-        <button @click="upgrade(1000,100000)" class="button" :disabled="juiceboxes <=99999">Buy 1000 cps for 100000 juiceboxes</button>
+    <section class="shop-container">
+        <h2 class="pad">Store</h2>
+        <div class="store-list">
+        </div>
+        <div class="columnye">
+        <button @click="upgrade(0.1,10)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=9" onclick="" >Buy 0.1 cps for 10 🧃</button>
+        <br>
+        <button @click="upgrade(0.3,50)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=49" onclick="" >Buy 0.3 cps for 50 🧃</button>
+        <br>
+        <button @click="upgrade(1,100)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=99">Buy 1 cps for 100 🧃</button>
+        <br>
+        <button @click="upgrade(3,500)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=499">Buy 3 cps for 500 🧃</button>
+        <br>
+        <button @click="upgrade(10,1000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=999">Buy 10 cps for 1k 🧃</button>
+        <br>
+        <button @click="upgrade(30,5000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=4999">Buy 30 cps for 5k 🧃</button>
+        <br>
+        <button @click="upgrade(100,10000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=9999">Buy 100 cps for 10k 🧃</button>
+        <br>
+        <button @click="upgrade(1000,100000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=99999">Buy 1k cps for 100k 🧃</button>
+        <br>
+        <button @click="upgrade(10000,1000000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=999999">Buy 10k cps for 1M 🧃</button>
+        <br>
+        <button @click="upgrade(100000,10000000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=9999999">Buy 100k cps for 10M 🧃</button>
+        <br>
+        <button @click="upgrade(1000000,100000000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=99999999">Buy 1M cps for 100M 🧃</button>
+        <br>
+        <button @click="upgrade(10000000,1000000000)" class="button is-warning is-medium is-fullwidth" :disabled="juiceboxes <=999999999">Buy 10M cps for 1B 🧃</button>
+        <br> 
     </div>
+    </section>
   </div>
+  <div class="juicebox_n_cps_background"></div>
+
+  <article class="message is-warning">
+  <div class="message-header">
+    <p>Tip #1</p>
+  </div>
+  <div class="message-body">
+    Et mahlapakke kokku hoida ja, et neid rohkem saada ära osta 0.3; 3 ega 30 cpsi!
+  </div>
+</article>
+
+<article class="message is-warning">
+  <div class="message-header">
+    <p>Tip #2</p>
+  </div>
+  <div class="message-body">
+    Kepi raha tee naisi - Sillu
+    <br>
+    Kõik panevad pihku ära ole eriline - Sillu
+  </div>
+</article>
+
 </template>
 
 <script>
+
+
+
 export default {
+
     created(){
         setInterval(() => {
             this.juiceboxes += this.cps;
@@ -52,14 +102,125 @@ export default {
                     this.juiceboxes -= cost;
                     this.cps += cps;
                 }
-            }
-        }
+            },
+        },
 }
 </script>
 
 <style scoped>
-    img.click {
-        transform: scale(1.5);
-        transition:0.5s;
+
+
+    .juicebox_n_cps_background {
+        background-color: rgba(0, 0, 0, 0.579);
+        width: 2000px;
+        height: 120px;
+        margin-top: -54.5%;
+        margin-left: -20%;
+        
     }
+
+    b {
+        color: white;
+        font-size: 24px;
+        
+    }
+    .column{
+        margin-left: 35%;
+        margin-top: -4%;
+    }
+
+    img{
+        margin-top: 10%;
+        transition: 1.5s;
+        opacity: 50%;
+        animation-name: floating;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-timing-function: ease-in-out;
+    }
+
+    img:hover{
+        transition: 0.6s;
+        opacity: 100%;
+    }
+    img.click {
+        transition: 1s;
+        animation: floating2;
+        animation-duration: 0.5s;
+        
+    }
+
+        .shop-container {
+        position:relative;
+        background: rgba(0, 0, 0, 0.377);
+        color: white;
+        width: 500px;
+        overflow-y: scroll;
+        height: 577.5px;
+        top: 61px;
+        left: 124px;
+        
+        
+        
+    }
+
+        .shop-container::before {
+            position: absolute;
+            content: "X";
+            top: 0;
+            left: 0;
+            padding: 15px 30px;
+            transform: translateX(-100%);
+            z-index: 1000;
+            
+        }
+
+        h2{
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+            background: #111;
+        }
+
+        .pad {
+            padding: 15px 30px;
+        }
+
+        .store-list {
+            display: flex;
+            flex-direction: column;
+            overflow-y:auto;
+        }
+
+        .message-body {
+            font-size: 18px;
+        }
+        .message {
+            margin-left: -11%;
+            width: 25%;
+            height: 200px;
+            transition: 1.5s;
+            opacity: 30%;
+        }
+
+        .message:hover {
+            transform: scale(1.1);
+            margin-left: 0.01%;
+            margin-top: 1%;
+            opacity: 100%;
+        }
+
+        @keyframes floating {
+    0% { transform: translate(0,  0px); }
+    50%  { transform: translate(0, 15px); }
+    100%   { transform: translate(0, -0px); }   
+}
+
+@keyframes floating2 {
+    0% { transform: translate(0,  0px); }
+    50%  { transform: translate(0, 10px); }
+    100%   { transform: translate(0, -0px); }   
+}
+
+
 </style>
---
+++ src/pages/RickAndMorty.vue
@@ -0,0 +1,58 @@
+<template>
+    <simple-pagination @prev="prev()" @next ="next()" :info="info"></simple-pagination>
+    <full-pagination :info="info" :current="currentPage" @goToPage="goToPage"></full-pagination>
+    <div class="columns is-multiline">
+        <div v-for="character in characters" :key="character.id" class="column is-one-quarter">
+            <character-card :character="character"></character-card>
+        </div>
+    </div>
+  
+</template>
+
+<script>
+    import axios from "axios";
+import CharacterCard from "../components/CharacterCard.vue";
+import SimplePagination from "../components/SimplePagination.vue";
+import FullPagination from "../components/FullPagination.vue";
+export default {
+    components: { CharacterCard, SimplePagination, FullPagination },
+    created() {
+        this.getPage('https://rickandmortyapi.com/api/character')
+    },
+    data() {
+        return {
+            info: {},
+            characters: [],
+            currentPage: 1
+        };
+    },
+    methods: {
+        
+        getPage(url){
+            axios.get(url).then(res => {
+                console.log(res.data);
+                this.info = res.data.info;
+                this.characters = res.data.results;
+            });
+        },
+        
+        prev(){
+            runInThisContext.currentPage--;
+            this.getPage(this.info.prev);
+    },
+    next(){
+        runInThisContext.currentPage++;
+        this.getPage(this.info.next);
+    },
+    goToPage(page){
+        this.currentPage = page;
+        this.getPage('https://rickandmortyapi.com/api/character' + page);
+    }
+}
+}
+
+</script>
+
+<style>
+
+</style>
